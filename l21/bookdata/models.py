7439cb39ba048df39342c492820ef1c585a4b802
dnxa <kuridze0@gmail.com>
2024-12-14 19:28:19 +0400

    Add models

--- l21/bookdata/models.py
+++ l21/bookdata/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.first_name)
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    release_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.title)
--- l21/bookdata/views.py
+++ l21/bookdata/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def all_authors_view(request):
+def all_view(request):
     pass
 
 def top_3_view(request):
